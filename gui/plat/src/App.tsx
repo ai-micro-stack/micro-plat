9d306a2281b376295052e0992f49aac7c8ae4901
big-data-consultant <big.data.consultant@outlook.com>
2025-11-27 18:40:03 -0500

    initial commit

--- gui/plat/src/App.tsx
+++ gui/plat/src/App.tsx
@@ -2,20 +2,23 @@ import type { NavLinkRenderProps } from "react-router";
 import { Routes, Route, NavLink } from "react-router";
 import { Container, Row, Col, Navbar, Nav, Button } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
+import { Suspense, lazy } from "react";
 import { AuthProvider, useAuth } from "@/components/AuthService";
 import { storeRefreshToken } from "@/utils/refreshToken";
 import { PrivateRoute } from "@/components/PrivateRoute";
-import HomePage from "@/pages/HomePage";
-import PlatManager from "@/pages/PlatManager";
-import PlatPlanner from "@/pages/PlatPlanner";
-import PlatBuilder from "@/pages/PlatBuilder";
-import PlatPlatform from "@/pages/PlatPlatform";
-import PlatPlugin from "@/pages/PlatPlugin";
-import TeamMember from "@/pages/TeamMember";
-import AboutUs from "@/pages/AboutUs";
-import UserProfile from "@/pages/UserProfile";
-import UserLogin from "@/pages/UserLogin";
-import Register from "@/pages/UserRegister";
+
+const HomePage = lazy(() => import("@/pages/HomePage"));
+const PlatManager = lazy(() => import("@/pages/PlatManager"));
+const PlatPlanner = lazy(() => import("@/pages/PlatPlanner"));
+const PlatBuilder = lazy(() => import("@/pages/PlatBuilder"));
+const PlatPlatform = lazy(() => import("@/pages/PlatPlatform"));
+const PlatPlugin = lazy(() => import("@/pages/PlatPlugin"));
+const TeamMember = lazy(() => import("@/pages/TeamMember"));
+const AboutUs = lazy(() => import("@/pages/AboutUs"));
+const UserProfile = lazy(() => import("@/pages/UserProfile"));
+const UserLogin = lazy(() => import("@/pages/UserLogin"));
+const Register = lazy(() => import("@/pages/UserRegister"));
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
@@ -29,16 +32,14 @@ const Navigation = () => {
   const navigate = useNavigate();
   return (
     <Navbar
-      // bg="dark"
       bg="primary"
       data-bs-theme="dark"
-      // sticky="top"
       className="nav"
     >
       <Container>
         <Navbar.Brand>
           <img
-            src="favicon.ico"
+            src="/favicon.ico"
             width="28"
             height="28"
             className="d-inline-block align-bottom me-2"
@@ -94,7 +95,7 @@ const Navigation = () => {
           <Button
             style={{
               backgroundColor: "transparent",
-              backgroundImage: "url('./profile.png')",
+              backgroundImage: "url('/profile.png')",
               backgroundSize: "cover",
               width: "40px",
               height: "40px",
@@ -140,7 +141,8 @@ const App = () => {
         <Col>
           <AuthProvider>
             <Navigation />
-            <Routes>
+            <Suspense fallback={<div>Loading...</div>}>
+              <Routes>
               <Route index element={<HomePage />} />
               <Route
                 path="plat-manager"
@@ -202,7 +204,8 @@ const App = () => {
               <Route path="user-login" element={<UserLogin />} />
               <Route path="register" element={<Register />} />
               <Route path="*" element={<p>There's nothing here: 404!</p>} />
-            </Routes>
+              </Routes>
+            </Suspense>
           </AuthProvider>
         </Col>
       </Row>
